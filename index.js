eb8671441a1ba552e99641cc04bcd6e47ca8ef09
Vijay Parmar <112509583+vijayparmar27@users.noreply.github.com>
2024-03-08 14:36:50 +0530

    add  code

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile("ok......!");
 });
 
 http.listen(3000, () => {
